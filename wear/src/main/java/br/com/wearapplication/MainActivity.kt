e69bd51580398c8244670bdd04608313c39d3d03
Julio Gomes <juliorgm@gmail.com>
2021-04-22 21:07:51 -0300

    Module Wear - MainActivity: Recuperação de dados do app celular e exibição na tela smartwatch

--- wear/src/main/java/br/com/wearapplication/MainActivity.kt
+++ wear/src/main/java/br/com/wearapplication/MainActivity.kt
@@ -1,15 +1,43 @@
 package br.com.wearapplication
 
+import android.app.Activity
 import android.os.Bundle
-import android.support.wearable.activity.WearableActivity
+import br.com.shared.Meal
+import br.com.wearapplication.databinding.ActivityMainBinding
+import com.google.android.gms.common.api.GoogleApiClient
+import com.google.android.gms.wearable.Wearable
+import com.google.gson.Gson
 
-class MainActivity : WearableActivity() {
+class MainActivity : Activity(), GoogleApiClient.ConnectionCallbacks {
+
+    private lateinit var client: GoogleApiClient
+    private var currentMeal: Meal? = null
+    lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        // Enables Always-on
-        setAmbientEnabled()
+
+    }
+
+    override fun onConnected(p0: Bundle?) {
+        Wearable.MessageApi.addListener(client) { messageEvent ->
+            currentMeal = Gson().fromJson(String(messageEvent.data), Meal::class.java)
+            updateView()
+        }
+    }
+
+    private fun updateView() {
+        currentMeal?.let {
+            binding.mealTitle.text = it.title
+            binding.caloriesWear.text = "Calorias: ${it.calories}"
+            binding.ingredientsWear.text = it.ingredients.joinToString { ", " }
+        }
+    }
+
+    override fun onConnectionSuspended(p0: Int) {
+        TODO("Not yet implemented")
     }
 }
